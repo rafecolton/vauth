19cdd78a41aa466ddf743c9e160e4bb57c433640
Rafe Colton <r.colton@modcloth.com>
2014-08-24 21:28:31 -0700

    Adding Travis and Github webhooks

---
+++ util.go
@@ -0,0 +1,10 @@
+package auth
+
+import (
+	"github.com/martini-contrib/auth"
+)
+
+// SecureCompare performs a constant time compare of two strings to limit timing attacks.
+func SecureCompare(given string, actual string) bool {
+	return auth.SecureCompare(given, actual)
+}
